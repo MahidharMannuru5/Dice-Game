ee89f224394b90040076302b3b9af310debe0a14
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2022-02-15 21:36:06 +0530

    Update js.js

--- js.js
+++ js.js
@@ -4,15 +4,15 @@ function RD()
 var x=Math.floor(Math.random()*6+1);
 var y=Math.floor(Math.random()*6+1);
 
-var a="C:\Users\LENOVO\OneDrive\Desktop\Dice\Dice-Game\images\dice"+x+".png";
+var a="images/dice"+x+".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", "C:/Users/LENOVO/OneDrive/Desktop/Dice/Dice-Game/images/dice1.png");
-var b="C:/Users/LENOVO/OneDrive/Desktop/Dice/Dice-Game/images/dice"+y+".png";
+document.querySelectorAll("img")[0].setAttribute("src", "images/dice1.png");
+var b="images/dice"+y+".png";
 document.querySelectorAll("img")[1].setAttribute("src", b);
 }
 function RD2()
 {
   var p=Math.floor(Math.random()*6+1);
-  var q="C:/Users/LENOVO/OneDrive/Desktop/Dice/Dice-Game/images/dice"+p+".png";
+  var q="images/dice"+p+".png";
   document.querySelectorAll("img")[2].setAttribute("src",q );
 }
